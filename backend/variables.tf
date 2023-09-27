8a585a994ebf10ff3f4ed5f0a67b0b7d0944b8de
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-27 22:34:50 +0200

    Creates the basae resources in the main config file and created the necessart variables

---
+++ backend/variables.tf
@@ -0,0 +1,29 @@
+variable "rg_backend_name" {
+    type = string
+    description = "The resource group name"
+  
+}
+variable "rg_backend_location" {
+      type = string
+      description = "The resource group location"
+
+}
+variable "sa_backend_name" {
+      type = string
+      description = "The storage account name"
+
+}
+variable "sc_name" {
+      type = string
+      description = "The storage account name"
+
+}
+variable "kv_backend_name" {
+      type = string
+      description = "The key vault name"
+}
+variable "sa_backend_ak_name" {
+      type = string
+      description = "The access key name"
+
+}
